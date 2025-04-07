9d95d8ae3b2b35c65cc549bccfec54d4b3cec1d5
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-07 08:42:46 -0300

    MarcaçãoConsultasMedicas

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Aula 31/03/2025
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fe3867',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
